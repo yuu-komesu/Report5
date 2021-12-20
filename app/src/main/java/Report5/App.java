e5222f32da71200e2e8cd268724908e5cf03ed41
Komesu YU <e215742@ie.u-ryukyu.ac.jp>
2021-12-20 20:52:11 +0900

    Use exception handling

--- app/src/main/java/Report5/App.java
+++ app/src/main/java/Report5/App.java
@@ -1,9 +1,14 @@
-
 package Report5;
+import java.lang.NullPointerException;
 
 public class App {
     public static void main(String[] args) {
+        try{
         String str = null;
         System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("入力がありません");
+            System.out.println(e.getMessage());
+        }
     }
 }
