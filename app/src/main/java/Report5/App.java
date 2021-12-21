4af9ea79e63060396e9b0b855427d18d2407047e
Komesu YU <e215742@ie.u-ryukyu.ac.jp>
2021-12-21 09:52:52 +0900

    Code of Step3

--- app/src/main/java/Report5/App.java
+++ app/src/main/java/Report5/App.java
@@ -1,14 +1,8 @@
 package Report5;
-import java.lang.NullPointerException;
 
 public class App {
     public static void main(String[] args) {
-        try{
-        String str = null;
-        System.out.println(str.length());
-        }catch (NullPointerException e){
-            System.out.println("入力がありません");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
